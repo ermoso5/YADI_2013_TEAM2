2644694a3d12fda36d570382debfe1906ea45007
Hussein <h.natsheh@ciapple.com>
2013-11-16 22:48:58 +0100

    Update db.py

--- db.py
+++ db.py
@@ -1,3 +1,12 @@
+'''
+Created on 16 nov. 2013
+
+@author: hussein
+'''
+import psycopg2 as dbapi2
+db = dbapi2.connect (database="datalog", user="postgres", password="dmkm")
+cur = db.cursor()
+
 class Predicate:
     alias=""
     def __init__(self, name, slots):
@@ -9,14 +18,34 @@ class slot:
         self.Value=Value                              ## in case of constant fill value here 
         self.variableName = variableName              ## in case of variable put the name here
          
+Pred=Predicate("Fact1",[slot("ABC",""),slot("123",""),slot("555","") ])
 
 class Database:
     def createTable(self,Pred):
-      re=1
-      ## 1- check for existance
-	  
-      ## 2- if not exists create table
-      ## 3- insert slots value as the arrangement order (get the slot.Value not slot.variableName )
-    
-        
-P=Predicate("Fact1",[slot("ABC",""),slot("123",""),slot("555","") ])
+        str="create table ("
+        str+= Pred.name
+        c=0
+        for i in Pred.slot:
+            str+= "col"+c.__str__+"  varchar ,"
+            c+=1
+        str=str[:-1]   ## remove the last comma    
+        str +=",id serial PRIMARY KEY)"
+        ##print(str)
+        cur.execute (str)
+        db.commit()
+        str1 ="INSERT INTO "
+        str1+= Pred.name
+        c=0
+        srt1 +="("
+        for j in Pred.slot:
+            st1r += "col"+c.__str__
+            c+=1
+        str1 +=") VALUES ("
+        c=0
+        for k in Pred.slot:
+            str1 +=Pred.slot(c).Value
+            c+=1
+        str1 +="))"
+        cur.execute (str1)
+        db.commit()
+       
