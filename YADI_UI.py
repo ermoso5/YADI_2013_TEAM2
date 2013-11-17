d6af4ea5221cd56a9ab3cf2418660277f6c6ed3c
Tajalla <tajalla_hashmi@hotmail.com>
2013-11-17 17:35:34 +0000

    Update YADI_UI.py

--- YADI_UI.py
+++ YADI_UI.py
@@ -1,16 +1,21 @@
 from tkinter import *
-from tkinter.ttk import *
+#from tkinter.ttk import *
 from Parser import *
 
 def load_rules():    
-    filename = filedialog.askopenfilename(filetypes=[('Text files', '*.txt')])
+    filename = filedialog.askopenfilename(filetypes=[('Text files', '*.txt')], initialdir = "C:/Python33")
     if filename: 
         try:
-            #Label(definition_mode, text=filename).pack()
-            print("dsf")
-            DatalogParser(filename)
+            rule_file = open(filename).read()
+            Label(rules, text=rule_file, bg="white", width=200).pack()
+            filename.close()
+            parser = DatalogParser(filename)
         except: 
             tkMessageBox.showerror("Open Source File", "Failed to read file \n'%s'"%filename) 
+
+def execute():
+    parser.setQuery(query_inp.get())
+    #Convert to SQL and evaluate
 
 #create the window
 master = Tk()
@@ -19,23 +24,38 @@ master = Tk()
 master.title("YADI")
 master.geometry("400x400")
 
-#set query mode
-query_mode = Frame(master)
-query_mode.pack(side=LEFT, fill=X)
 #set definition mode
-definition_mode = Frame(master)
+
+definition_mode = Frame(master, width=400)
+definition_mode.pack()
+
+Label(definition_mode, text="Rules").pack()
+
+rules = Frame(definition_mode, width=400, height=200, bg="white")
+rules.pack()
+
 rule_loader = Button(definition_mode, text="Select your file", command=load_rules)
-rule_loader.pack(side=LEFT)
-definition_mode.pack(side=LEFT, fill=X)
-   
+rule_loader.pack(side=RIGHT)
 
-rules_text = Message(definition_mode)
-query_text = Message(definition_mode)
+#set query mode
+query_mode = Frame(master, width=400, height=80)
+query_mode.pack()
 
-execute_query = Button(definition_mode, text="Execute")
-execute_query.pack(side=RIGHT)
-definition_mode.pack(side=RIGHT, fill=X)
+query_lb = Label(query_mode, text="Query").pack()
+query_inp = Entry(query_mode, width=200, bg="white")
+query_inp.pack()
 
-results_text = Message(definition_mode,)
+executer = Button(query_mode, text="Execute", command=execute)
+executer.pack(side=RIGHT)
+
+#results
+results = Frame(master, width=400)
+results.pack()
+
+Label(results, text="Results").pack()
+
+result_block = Frame(results, width=300, height=60, bg="white")
+result_block.pack()
 
 master.mainloop()
+
