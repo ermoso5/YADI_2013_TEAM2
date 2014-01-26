69a3ecb18e152ad7e154d883fac4fcef4fcad538
Qutiba <qinformatic@gmail.com>
2014-01-26 23:29:48 +0100

    Re

--- Main.py
+++ Main.py
@@ -6,66 +6,32 @@ from pyparsing import *
 from MainPKG.Parser import *
 from MainPKG.Evaluator import *
 from MainPKG.DatabaseConnection import *
-#===============================================================================
-#
-# initClass=initialization_class()
-# initClass.intiatlize_console()
-# #print("Please enter the Rule file path : ")
-# #path=sys.stdin.readline()
-# path = 'C:/Python33/Rules.txt' #path[:-1]
-#
-# DatalogParser1= DatalogParser(path)
-#
-# Rs=DatalogParser1.GetRules()
-# EC=Evalute()
-# query="aV(X,Y)"
-# F=DatalogParser1.toPredicate(Grammar().literal.parseString(query))
-#===============================================================================
-
-#===============================================================================
-# #initClass.DB_name="Datalog"
-# #initClass.DB_user="postgres"
-# #initClass.DB_password="a111111b"
-# #initClass.Connet()
-# #@"C:\\Users\\qutiba\\Desktop\\Rules.txt"
-#
-# for R in Rs:
-#     if R.Head.Name==F.Name:
-#         EC.evalute(R)
-#         print()
-#         print("Query is ")
-#         print(EC.sql_tables+" where "+EC.sql_condition)
-#         break
-#
-# DB=Database()
-#
-# print()
-# print()
-#
-# Rows=initClass.DB.Select(EC.sql_tables+" where "+EC.sql_condition)
-# for R in Rows:
-#     print(R)
-#===============================================================================
 
 
-
-######QUTIBA'S TEST
 
 
 initClass=initialization_class()
-initClass.intiatlize_console()
-#initClass.DB_name="Datalog"
-#initClass.DB_user="postgres"
-#initClass.DB_password="a111111b"
-#path = "C:\\Users\\qutiba\\Desktop\\Rule1.txt"
+#initClass.intiatlize_console()
+initClass.DB_name="Datalog"
+initClass.DB_user="postgres"
+initClass.DB_password="a111111b"
+initClass.path = "C:\\Users\\qutiba\\Desktop\\Rule1.txt"
 initClass.Connet()
 DatalogParser1= DatalogParser(initClass.path)
-#query="student_book_age('student3',F,'20')"
-query="reach(A,'G')"
-#try:
+initClass.Query="reach(A,'G')"
 EC=Evalutor(DatalogParser1,initClass)
-#query="student_book(D,V)"
-EC.Execute(query)
-#EC.Drop_created_views()
-
-
+while true:
+    print("Please enter the qurey : ")
+    initClass.Query=sys.stdin.readline()
+    initClass.Query=initClass.Query[:-1]
+    if initClass.Query == "-1":
+        sys.exit()
+    
+    EC.PrepareRules_create_views_for_primary_rules()
+    EC.Execute(initClass.Query)
+    #     student_book_age(A,D,R)    
+        
+    
+    
+    
+    
